07326f12f9be16f13a5c94034a2999b090d01009
sernamalala <sernaprograms@gmail.com>
2024-02-21 23:00:09 +0200

    python exercise week 1

---
+++ Week 1/user_input.py
@@ -0,0 +1,7 @@
+# ask user for their name
+
+name = input("What is your name?")
+age = input("How old are you?")
+location = input("What country and city do you live in?")
+
+print(f"Hello {name}, you are {age} years old and live in {location}.")
