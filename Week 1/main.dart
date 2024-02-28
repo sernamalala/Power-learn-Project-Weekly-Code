7bbf33e9f3afc5498a973fffee9a89181ede2534
sernamalala <sernaprograms@gmail.com>
2024-02-28 22:27:28 +0200

    Week 2 web dev exercise with links and images

---
+++ Week 1/main.dart
@@ -0,0 +1,11 @@
+void main() {
+  print("Hello World");
+
+  int age = 20;
+  String name = "Serna";
+  String surname = "Malala";
+  bool isFemale = true;
+
+  print(
+      "Welcome $name $surname. You have joined the girls cohort for young adults. You have qualified because you are $age years old");
+}
