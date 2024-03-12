a69d8fc824007bd8e7a226473a9378d3f6a1e0f2
sernamalala <sernaprograms@gmail.com>
2024-03-12 13:09:27 +0200

    week 4. html forms

--- Week 2/weekly_challenge.py
+++ Week 2/weekly_challenge.py
@@ -40,3 +40,7 @@ person["age"] = age
 person["favourite colour"] = colour
 
 print(person)
+
+#4
+#Write a program that accepts user input to create two sets of integers. Then, create a new set that contains only the elements that are common to both sets.
+
